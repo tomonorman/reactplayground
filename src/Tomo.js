f0918f903a63e156d89744288acfc27e956a09ad
Tomo Norman <tomo.l.norman@gmail.com>
2020-12-17 09:10:11 +0900

    Tomo component created

---
+++ src/Tomo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Tomo extends Component {
+  render() {
+    return(
+      <div className="tomo">
+        <div>Name: Tomo</div>
+        <div>Age: 35</div>
+        <div>Hobby: Wine</div>
+      </div>
+    );
+  }
+}
+
+export default Tomo;
