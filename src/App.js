525c43401a33df0cab9a6f8b1e14c4bb5bdd25d0
Tomo Norman <tomo.l.norman@gmail.com>
2020-12-17 09:24:14 +0900

    desctructured props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React, { Component } from 'react';
 import Tomo from './Tomo';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My first React App</h1>
-      <p>Welcome!</p>
-      <Tomo />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <h1>My first React App</h1>
+        <p>Welcome!</p>
+        <Tomo name="Tomo" age="35" hobby="wine" />
+        <Tomo name="Mia" age="1" hobby="ball" />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Tomo.js
+++ src/Tomo.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 class Tomo extends Component {
   render() {
+    const { name, age, hobby } = this.props;
     return(
       <div className="tomo">
-        <div>Name: Tomo</div>
-        <div>Age: 35</div>
-        <div>Hobby: Wine</div>
+        <div>Name: {name}</div>
+        <div>Age: {age}</div>
+        <div>Hobby: {hobby}</div>
       </div>
     );
   }
