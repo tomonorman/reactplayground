d190b5e6629b8cf8f90d1c70b97eab5ff0962b00
Tomo Norman <tomo.l.norman@gmail.com>
2020-12-17 10:47:02 +0900

    conditional added for age

--- src/Tomo.js
+++ src/Tomo.js
@@ -1,24 +1,37 @@
-import React, { Component } from 'react';
+import React  from 'react';
 
-class Tomo extends Component {
-  render() {
-    const { people } = this.props;
-    const peopleList = people.map(person => {
-      return(
-        <div className="tomo" key={person.id}>
-          <div>Name: {person.name}</div>
-          <div>Age: {person.age}</div>
-          <div>Hobby: {person.hobby}</div>
-        </div>
-      );
-    })
-    // Output to App.js
-    return(
-      <div className="people-list">
-        { peopleList } 
+const Tomo = ({people}) => {
+
+  // using if statement
+  // const peopleList = people.map(person => {
+  //   if (person.age > 20) {
+  //     return(
+  //       <div className="tomo" key={person.id}>
+  //         <div>Name: {person.name}</div>
+  //         <div>Age: {person.age}</div>
+  //         <div>Hobby: {person.hobby}</div>
+  //       </div>
+  //     );
+  //   } else {
+  //     return null;
+  //   }
+  // })
+  //using ternary operator
+  const peopleList = people.map(person => {
+    return person.age > 20 ? (
+      <div className="tomo" key={person.id}>
+        <div>Name: {person.name}</div>
+        <div>Age: {person.age}</div>
+        <div>Hobby: {person.hobby}</div>
       </div>
-    );
-  }
+    ) : null;
+  })
+  // Output to App.js
+  return(
+    <div className="people-list">
+      { peopleList } 
+    </div>
+  );
 }
 
 export default Tomo;
